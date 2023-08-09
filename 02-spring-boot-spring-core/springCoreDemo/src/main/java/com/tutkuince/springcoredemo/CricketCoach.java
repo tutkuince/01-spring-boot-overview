ce91bddfd79d0adbf91ee11f1dc772de7eaa2222
Tutku Ince <tutku.ince@outlook.com>
2023-08-09 19:21:59 +0300

    Demo controller and Cricket coach

--- 02-spring-boot-spring-core/springCoreDemo/src/main/java/com/tutkuince/springcoredemo/CricketCoach.java
+++ 02-spring-boot-spring-core/springCoreDemo/src/main/java/com/tutkuince/springcoredemo/CricketCoach.java
@@ -7,6 +7,6 @@ public class CricketCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes";
+        return "Practice fast bowling for 15 minutes!";
     }
 }
