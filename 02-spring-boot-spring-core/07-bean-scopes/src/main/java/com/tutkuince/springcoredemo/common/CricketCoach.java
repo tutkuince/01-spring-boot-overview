4341a1e5346e3ccfdce6e4f0359ecf270bf18b74
Tutku Ince <tutku.ince@outlook.com>
2023-08-11 23:19:14 +0300

    bean scope demo

--- 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/tutkuince/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/tutkuince/springcoredemo/common/CricketCoach.java
@@ -1,8 +1,12 @@
 package com.tutkuince.springcoredemo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     @Override
--- 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/tutkuince/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/tutkuince/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.tutkuince.springcoredemo.rest;
 
 import com.tutkuince.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +11,24 @@ public class DemoController {
 
     // Define a private field for the dependency
     private final Coach myCoach;
+    private final Coach anotherCoach;
 
     @Autowired
-    public DemoController(Coach myCoach) {
+    public DemoController(
+            @Qualifier("cricketCoach") Coach myCoach,
+            @Qualifier("cricketCoach") Coach anotherCoach) {
+        System.out.println("In Constructor: " + getClass().getSimpleName());
         this.myCoach = myCoach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyWorkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 }
