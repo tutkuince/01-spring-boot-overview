4c954ce9347b8ac1763551c3d0f204744795508c
Tutku Ince <tutku.ince@outlook.com>
2023-08-11 22:47:18 +0300

    lazy initialization starter

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/tutkuince/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.tutkuince.springcoredemo.rest;
+
+import com.tutkuince.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // Define a private field for the dependency
+    private final Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
