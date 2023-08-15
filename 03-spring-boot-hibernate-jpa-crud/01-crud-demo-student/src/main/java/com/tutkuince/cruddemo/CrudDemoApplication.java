a6f7cb5a3a486e754ce644c1ff655736b6b961a0
Tutku Ince <tutku.ince@outlook.com>
2023-08-15 17:11:14 +0300

    saving Java Object with JPA demo

--- 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/tutkuince/cruddemo/CrudDemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/tutkuince/cruddemo/CrudDemoApplication.java
@@ -1,5 +1,7 @@
 package com.tutkuince.cruddemo;
 
+import com.tutkuince.cruddemo.dao.StudentDAO;
+import com.tutkuince.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,22 @@ public class CrudDemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("Hello World!");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        // create the student object
+        System.out.println("Creating new Student Object . . .");
+        Student tempStudent = new Student("Tutku", "Ince", "ti@tutkuince.com");
+
+        // save the student object
+        System.out.println("Saving the Student . . .");
+        studentDAO.save(tempStudent);
+
+        // display id of the saved student
+        System.out.println("Saved student. Generated id: " + tempStudent.getId());
     }
 }
---
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/tutkuince/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.tutkuince.cruddemo.dao;
+
+import com.tutkuince.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
