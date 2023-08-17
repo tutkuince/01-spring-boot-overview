7c61b8021ec53dbbb7bc0dabcc55a7f4c19beeb8
Tutku Ince <tutku.ince@outlook.com>
2023-08-17 14:55:14 +0300

    check student is found or not, create new response for error message

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/tutkuince/springrest/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/tutkuince/springrest/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.tutkuince.springrest.rest;
 
 import com.tutkuince.springrest.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +32,25 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        // check the studentId again list size
+        if (studentId >= theStudents.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
         return theStudents.get(studentId);
+    }
+
+    // Add an exception handler using @ExceptionHanler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // Return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
