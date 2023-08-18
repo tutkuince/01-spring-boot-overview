47c149ec8c05506b6dab6bcfc2302649391e6d69
Tutku Ince <tutku.ince@outlook.com>
2023-08-18 23:02:35 +0300

    EmployeeRestController has been refactored

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/tutkuince/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/tutkuince/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,7 +1,7 @@
 package com.tutkuince.springboot.cruddemo.rest;
 
-import com.tutkuince.springboot.cruddemo.dao.EmployeeDAO;
 import com.tutkuince.springboot.cruddemo.entity.Employee;
+import com.tutkuince.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +13,15 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public EmployeeRestController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
