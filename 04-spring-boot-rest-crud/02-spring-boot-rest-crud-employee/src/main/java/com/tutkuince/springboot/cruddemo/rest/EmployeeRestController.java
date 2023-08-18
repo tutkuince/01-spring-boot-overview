bcf6b51e3b2a2caae8273c394ed21d1788c90234
Tutku Ince <tutku.ince@outlook.com>
2023-08-18 22:46:12 +0300

    EmployeeRestController has been created

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/tutkuince/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,27 @@
+package com.tutkuince.springboot.cruddemo.rest;
+
+import com.tutkuince.springboot.cruddemo.dao.EmployeeDAO;
+import com.tutkuince.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private final EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
