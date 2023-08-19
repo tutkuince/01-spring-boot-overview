77ae520ad3cad9814354a82adb53e4ad3124e3a8
Tutku Ince <tutku.ince@outlook.com>
2023-08-19 17:58:07 +0300

    spring data rest demo

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/tutkuince/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/tutkuince/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.tutkuince.springboot.cruddemo.dao;
 
 import com.tutkuince.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
