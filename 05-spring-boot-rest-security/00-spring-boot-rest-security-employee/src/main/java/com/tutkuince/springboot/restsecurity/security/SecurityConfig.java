f2a99a205612f3005b2fdea220b30875f263d03b
Tutku Ince <tutku.ince@outlook.com>
2023-08-22 21:58:10 +0300

    JDBC Authentication - Plain Text has been used

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee/src/main/java/com/tutkuince/springboot/restsecurity/security/SecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee/src/main/java/com/tutkuince/springboot/restsecurity/security/SecurityConfig.java
@@ -9,11 +9,16 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
@@ -35,6 +40,13 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+    */
+
+    // add support for JDBC . . . no more hardcoded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
