d43b1cd5195671f3b7fecec58b33fe6aced3b2d9
Tutku Ince <tutku.ince@outlook.com>
2023-08-22 20:49:34 +0300

    basic security configuration with creating users

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee/src/main/java/com/tutkuince/springboot/restsecurity/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.tutkuince.springboot.restsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test112")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test112")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test112")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+}
