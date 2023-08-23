57ca6e6d1f054fe27969cc0dc14ac444751a98e0
Tutku Ince <tutku.ince@outlook.com>
2023-08-23 21:08:00 +0300

    make integer fields required

--- 06-spring-boot-spring-mvc/02-thymeleaf-validation-demo/src/main/java/com/tutkuince/springboot/thymeleafdemo/entity/Customer.java
+++ 06-spring-boot-spring-mvc/02-thymeleaf-validation-demo/src/main/java/com/tutkuince/springboot/thymeleafdemo/entity/Customer.java
@@ -8,9 +8,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "min character size must be bigger that 1")
     private String lastName;
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -31,11 +32,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
