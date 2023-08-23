5ce708d5f2a475b04b7adee82fa0b3f1e7a86704
Tutku Ince <tutku.ince@outlook.com>
2023-08-23 16:14:31 +0300

    helloWorld demo for Thymelaef

---
+++ 06-spring-boot-spring-mvc/01-thymeleaf-demo/src/main/java/com/tutkuince/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.tutkuince.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+
+@Controller
+public class DemoController {
+
+    // create a mapping for "/hello"
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("theDate", new Date());
+        return "helloWorld";
+    }
+}
