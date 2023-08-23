661b3fe0732a1166c1af25341f2cf45056af1564
Tutku Ince <tutku.ince@outlook.com>
2023-08-23 21:56:28 +0300

    Custom validation demo

---
+++ 06-spring-boot-spring-mvc/02-thymeleaf-validation-demo/src/main/java/com/tutkuince/springboot/thymeleafdemo/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,18 @@
+package com.tutkuince.springboot.thymeleafdemo.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+    @Override
+    public void initialize(CourseCode courseCode) {
+        this.coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String code, ConstraintValidatorContext constraintValidatorContext) {
+        return code == null || code.startsWith(coursePrefix);
+    }
+}
