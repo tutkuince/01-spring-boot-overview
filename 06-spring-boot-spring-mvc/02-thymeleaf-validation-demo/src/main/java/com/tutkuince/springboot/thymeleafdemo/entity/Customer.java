a88056cd406508977f0b17064a3daa0ca1912236
Tutku Ince <tutku.ince@outlook.com>
2023-08-23 21:58:21 +0300

    custom validation demo

--- 06-spring-boot-spring-mvc/02-thymeleaf-validation-demo/src/main/java/com/tutkuince/springboot/thymeleafdemo/entity/Customer.java
+++ 06-spring-boot-spring-mvc/02-thymeleaf-validation-demo/src/main/java/com/tutkuince/springboot/thymeleafdemo/entity/Customer.java
@@ -17,7 +17,7 @@ public class Customer {
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
 
-    @CourseCode
+    @CourseCode(value = "TOPS", message = "must start with TOPS")
     private String courseCode;
 
     public String getFirstName() {
