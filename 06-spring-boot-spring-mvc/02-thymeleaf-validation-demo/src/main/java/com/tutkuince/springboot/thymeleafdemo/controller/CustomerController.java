6f30b76450f14151436dfcb69c20ec783f75e6d0
Tutku Ince <tutku.ince@outlook.com>
2023-08-23 18:22:32 +0300

    InitBinder demo

--- 06-spring-boot-spring-mvc/02-thymeleaf-validation-demo/src/main/java/com/tutkuince/springboot/thymeleafdemo/controller/CustomerController.java
+++ 06-spring-boot-spring-mvc/02-thymeleaf-validation-demo/src/main/java/com/tutkuince/springboot/thymeleafdemo/controller/CustomerController.java
@@ -2,15 +2,27 @@ package com.tutkuince.springboot.thymeleafdemo.controller;
 
 import com.tutkuince.springboot.thymeleafdemo.entity.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // add an initBinder annotation to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
