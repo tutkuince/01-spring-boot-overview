51f88c06f74bd8af63cca9b81c3c14afcd813363
Tutku Ince <tutku.ince@outlook.com>
2023-08-24 19:36:00 +0300

    html page

--- 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/controller/EmployeeController.java
+++ 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/controller/EmployeeController.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/employees")
 public class EmployeeController {
     private final EmployeeService employeeService;
     @Autowired
@@ -14,7 +16,7 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/employees")
+    @GetMapping("/list")
     public String getEmployees(Model model) {
         model.addAttribute("employeeList", employeeService.findAll());
         return "list-employees";
