c0ec1cf4d2e7f635552b6656a7e7aee898687c98
Tutku Ince <tutku.ince@outlook.com>
2023-08-24 21:16:49 +0300

    add employee sample

--- 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/dao/EmployeeRepository.java
+++ 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/dao/EmployeeRepository.java
@@ -4,6 +4,10 @@ import com.tutkuince.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    // add a method to sort by last name
+    List<Employee> findAllByOrderByLastNameAsc();
 }
