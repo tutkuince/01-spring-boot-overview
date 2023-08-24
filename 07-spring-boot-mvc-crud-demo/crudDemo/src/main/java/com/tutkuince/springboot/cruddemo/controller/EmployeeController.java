b29761bd07f807b224ee5350879e5de88dfb6e14
Tutku Ince <tutku.ince@outlook.com>
2023-08-24 21:25:04 +0300

    update employee

--- 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/controller/EmployeeController.java
+++ 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.tutkuince.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/employees")
@@ -37,5 +34,12 @@ public class EmployeeController {
     public String saveEmployee(@ModelAttribute Employee employee) {
         employeeService.save(employee);
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+        Employee employee = employeeService.findById(id);
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
     }
 }
