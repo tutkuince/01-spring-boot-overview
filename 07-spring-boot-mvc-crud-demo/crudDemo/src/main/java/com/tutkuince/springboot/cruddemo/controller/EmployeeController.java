e56764227a5c60b59b3eea30670e34036e500a50
Tutku Ince <tutku.ince@outlook.com>
2023-08-24 21:33:22 +0300

    delete employee demo

--- 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/controller/EmployeeController.java
+++ 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/controller/EmployeeController.java
@@ -42,4 +42,10 @@ public class EmployeeController {
         model.addAttribute("employee", employee);
         return "employees/employee-form";
     }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("employeeId") int id) {
+        employeeService.deleteById(id);
+        return "redirect:/employees/list";
+    }
 }
