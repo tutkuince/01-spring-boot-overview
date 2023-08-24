62ac0e13d22a16836080bb7b4c9f31064ab5be16
Tutku Ince <tutku.ince@outlook.com>
2023-08-24 21:03:44 +0300

    update

--- 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/controller/EmployeeController.java
+++ 07-spring-boot-mvc-crud-demo/crudDemo/src/main/java/com/tutkuince/springboot/cruddemo/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.tutkuince.springboot.cruddemo.controller;
 
+import com.tutkuince.springboot.cruddemo.entity.Employee;
 import com.tutkuince.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +22,20 @@ public class EmployeeController {
     @GetMapping("/list")
     public String getEmployees(Model model) {
         model.addAttribute("employeeList", employeeService.findAll());
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees/list";
     }
 }
