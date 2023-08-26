4d3e0373cfc89da57a84815cf5183f9b9baf71ca
Tutku Ince <tutku.ince@outlook.com>
2023-08-26 22:53:24 +0300

    setup spring advanced mapping project

--- 09-spring-boot-advanced-hibernate-mappings/00-jpa-one-to-one-uni/src/main/java/com/tutkuince/springboot/advancedjpa/AdvancedJpaApplication.java
+++ 09-spring-boot-advanced-hibernate-mappings/00-jpa-one-to-one-uni/src/main/java/com/tutkuince/springboot/advancedjpa/AdvancedJpaApplication.java
@@ -1,13 +1,22 @@
 package com.tutkuince.springboot.advancedjpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdvancedJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdvancedJpaApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello World!");
+        };
     }
 
 }
