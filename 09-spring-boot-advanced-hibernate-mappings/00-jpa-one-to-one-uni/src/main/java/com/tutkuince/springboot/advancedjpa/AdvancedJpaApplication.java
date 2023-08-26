48babe3dedb68d3be8153edab81d2af15852c6f5
Tutku Ince <tutku.ince@outlook.com>
2023-08-26 22:45:54 +0300

    one to one initializer

---
+++ 09-spring-boot-advanced-hibernate-mappings/00-jpa-one-to-one-uni/src/main/java/com/tutkuince/springboot/advancedjpa/AdvancedJpaApplication.java
@@ -0,0 +1,13 @@
+package com.tutkuince.springboot.advancedjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedJpaApplication.class, args);
+    }
+
+}
