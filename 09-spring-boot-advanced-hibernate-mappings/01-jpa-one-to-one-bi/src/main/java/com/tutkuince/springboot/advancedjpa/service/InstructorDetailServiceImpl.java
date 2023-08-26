3ff6aa8b1d81f5887431b17dc5416bf3079501ba
Tutku Ince <tutku.ince@outlook.com>
2023-08-27 00:40:13 +0300

    @Transactional annotation has been added for missing service methods

--- 09-spring-boot-advanced-hibernate-mappings/01-jpa-one-to-one-bi/src/main/java/com/tutkuince/springboot/advancedjpa/service/InstructorDetailServiceImpl.java
+++ 09-spring-boot-advanced-hibernate-mappings/01-jpa-one-to-one-bi/src/main/java/com/tutkuince/springboot/advancedjpa/service/InstructorDetailServiceImpl.java
@@ -4,6 +4,7 @@ import com.tutkuince.springboot.advancedjpa.dao.InstructorDetailRepository;
 import com.tutkuince.springboot.advancedjpa.entity.InstructorDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -18,6 +19,7 @@ public class InstructorDetailServiceImpl implements InstructorDetailService {
     }
 
     @Override
+    @Transactional
     public void save(InstructorDetail instructorDetail) {
         instructorDetailRepository.save(instructorDetail);
     }
@@ -35,6 +37,7 @@ public class InstructorDetailServiceImpl implements InstructorDetailService {
     }
 
     @Override
+    @Transactional
     public void deleteById(int id) {
         instructorDetailRepository.deleteById(id);
     }
