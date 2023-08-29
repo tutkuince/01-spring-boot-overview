96bd95d6bf377b74f55394b5e0377486c6686d21
Tutku Ince <tutku.ince@outlook.com>
2023-08-29 15:17:07 +0300

    Spring AOP Project setup has been completed

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/tutkuince/springboot/aopdemo/AopDemoApplication.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/tutkuince/springboot/aopdemo/AopDemoApplication.java
@@ -1,7 +1,9 @@
 package com.tutkuince.springboot.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopDemoApplication {
@@ -10,4 +12,10 @@ public class AopDemoApplication {
         SpringApplication.run(AopDemoApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello World!");
+        };
+    }
 }
