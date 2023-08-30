3c4ea780356333b6bb89108288c6f5c8e6d4c69b
Tutku Ince <tutku.ince@outlook.com>
2023-08-30 18:15:59 +0300

    Read Method Arguments with JoinPoints

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/tutkuince/springboot/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/tutkuince/springboot/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,11 @@
 package com.tutkuince.springboot.aopdemo.aspect;
 
+import com.tutkuince.springboot.aopdemo.entity.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 @Aspect
@@ -11,7 +14,24 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("com.tutkuince.springboot.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======>>> Executing @Before advice on addAccount()");
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
+        System.out.println("\n======>>> Executing @Before advice on method()");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
+
+        // display method arguments
+        Object[] args = joinPoint.getArgs();
+        for (Object obj :
+                args) {
+            System.out.println(obj);
+            if (obj instanceof Account) {
+                // downcast and print Account specific stuff
+                Account account = (Account) obj;
+                System.out.println("Account Name: " + account.getName());
+                System.out.println("Account Level: " + account.getLevel());
+            }
+        }
     }
 }
